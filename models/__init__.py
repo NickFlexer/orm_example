5928bfb5dfb312ac881b8137b0157396f92e3546
Val4ik <kvurevna@gmail.com>
2019-03-14 11:11:09 +0300

    add model files

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .hotel_data import db
---
+++ models/hotel_data.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+
+from pony.orm import *
+
+
+db = Database()
+
+
+class RoomType(db.Entity):
+    max_persone = Required(int)
+    description = Required(str, unique=True)
+    rooms = Optional("Rooms")
+
+
+class Cities(db.Entity):
+    name = Required(str, unique=True)
+    hotels = Set("Hotels")
+
+
+class Hotels(db.Entity):
+    name = Required(str, unique=True)
+    star_rating = Required(int)
+    city = Optional(Cities)
+    rooms = Optional("Rooms")
+
+
+class Rooms(db.Entity):
+    hotel = Required("Hotels")
+    type = Required("RoomType")
+    price = Required(Decimal)
+    is_booked = Required(bool)
